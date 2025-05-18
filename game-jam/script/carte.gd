a11ed73ddaf059efb440e2d7b9851f6892aec7c9
Raphael-code <65950772+Raphael-code@users.noreply.github.com>
2025-05-18 10:47:37 +0200

    hand mechanic finished

--- game-jam/script/carte.gd
+++ game-jam/script/carte.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+signal hover
+signal hover_off
+
+var starting_position
+
+func _ready() -> void:
+	get_parent().connect_card_signals(self)
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hover", self)
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hover_off", self)
